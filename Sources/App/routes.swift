2ba205c3d1a0a8be6ae81c54a6630397a272bf1c
Lailo <lailo@helio.exchange>
2020-04-11 17:35:46 +0200

    adds routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,37 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
-    }
+  app.get { _ in
+    "It works!"
+  }.description("says it works")
 
-    app.get("hello") { req -> String in
-        return "Hello, Vapor 4!"
+  app.group("hello") { hello in
+    hello.get { _ -> String in
+      "Hello, world!"
+    }.description("says hello world")
+
+    hello.get("vapor") { _ in
+      "Hello, vapor!"
+    }.description("says hello vapor")
+
+    hello.get(":name") { req -> String in
+      let name = req.parameters.get("name")!
+      return "Hello, \(name)!!"
+    }.description("says hello to a given name")
+  }
+
+  app.get("say", ":greeting", "to", ":name") { req -> String in
+    let greeting = req.parameters.get("greeting")!
+    let name = req.parameters.get("name")!
+    return "\(greeting), \(name)!"
+  }.description("says a custom greeting to a given name")
+
+  app.get("double", ":number") { req -> String in
+    guard let number = req.parameters.get("number", as: Int.self) else {
+      throw Abort(.badRequest)
     }
+    return "double of \(number) is \(2 * number)"
+  }.description("doubles a number or trhows error if there is no number")
+
+  print(app.routes.all)
 }
